277664f4648c81f6d17cb247bffb0e34e307d8a4
CervinskiV <cervinskiv26@hartfordschools.net>
2025-04-30 09:33:22 -0400

    Added connections between html and js

--- script.js
+++ script.js
@@ -7,5 +7,13 @@ let sysadmin
 document.addEventListener("submit", function(event) {
     event.preventDefault();
     cpus = document.getElementById("CPUs").value;
+    memory = document.getElementById("memory").value;
+    disk = document.getElementById("disk_space").value;
+    dartfs = document.getElementById("dartfs").value;
+    sysadmin = document.getElementById("sysadmin").checked;
     console.log(cpus)
+    console.log(memory)
+    console.log(disk)
+    console.log(dartfs)
+    console.log(sysadmin)
 });
