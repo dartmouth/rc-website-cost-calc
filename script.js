a2fcdac42722b8290f4bc6e338a812c195e08769
Lucas Prignano <prignanol26@hartfordschools.net>
2025-04-30 10:06:56 -0400

    added values to url after submission

--- script.js
+++ script.js
@@ -4,16 +4,10 @@ let disk
 let dartfs
 let sysadmin
 
-document.addEventListener("submit", function(event) {
-    event.preventDefault();
+function form_submitted() {
     cpus = document.getElementById("CPUs").value;
     memory = document.getElementById("memory").value;
     disk = document.getElementById("disk_space").value;
     dartfs = document.getElementById("dartfs").value;
     sysadmin = document.getElementById("sysadmin").checked;
-    console.log(cpus)
-    console.log(memory)
-    console.log(disk)
-    console.log(dartfs)
-    console.log(sysadmin)
-});
+}
