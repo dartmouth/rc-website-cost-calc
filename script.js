363f6ff33848f94afe9eb0699fc3d2564ab6b464
Lucas Prignano <prignanol26@hartfordschools.net>
2025-05-07 10:15:55 -0400

    added price calculations

--- script.js
+++ script.js
@@ -5,16 +5,37 @@ let dartfs
 let sysadmin
 
 function form_submitted() {
-    cpus = document.getElementById("CPUs").value;
-    memory = document.getElementById("memory").value;
-    disk = document.getElementById("disk_space").value;
+    cpus = Number(document.getElementById("CPUs").value);
+    memory = Number(document.getElementById("memory").value);
+    disk = Number(document.getElementById("disk_space").value);
     disk = Number(disk);
     console.log(typeof disk === "number");
-    dartfs = document.getElementById("dartfs").value;
-    sysadmin = document.getElementById("sysadmin").checked;
-    cost_per_cpu = 60;
-    cost_per_memory = 40;
-    cost_per_disk = 2.5;
-    cost_per_dartfs = 95;
-    sysadmin_cost = 200;
+    dartfs = Number(document.getElementById("dartfs").value);
+    sysadmin = Boolean(document.getElementById("sysadmin").checked);
+    console.log(price_calculation(cpus, memory, disk, dartfs, sysadmin));
+};
+
+function price_calculation(cpus, memory, disk, dartfs, sysadmin) {
+    const cost_per_cpu = 60;
+    const cost_per_gb_memory = 40;
+    const cost_per_gb_disk = 2.5;
+    const cost_per_tb_dartfs = 95;
+    const sysadmin_cost = 200;
+    let total_cost;
+    if (cpus != 0) {
+        total_cost += cost_per_cpu * cpus;
+    };
+    if (memory != 0) {
+        total_cost += cost_per_gb_memory * memory;
+    };
+    if (disk != 0) {
+        total_cost += cost_per_gb_disk * disk;
+    };
+    if (dartfs != 0) {
+        total_cost += cost_per_tb_dartfs * dartfs;
+    };
+    if (sysadmin == true) {
+        total_cost += sysadmin_cost;
+    };
+    return total_cost;
 };
