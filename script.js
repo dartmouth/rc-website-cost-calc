d3b0f93fc45fff8d196e3a3b2a3fccd3c8741a52
Lucas Prignano <prignanol26@hartfordschools.net>
2025-05-07 09:09:30 -0400

    added semicolons

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function form_submitted() {
     cpus = document.getElementById("CPUs").value;
     memory = document.getElementById("memory").value;
     disk = document.getElementById("disk_space").value;
-    disk = Number(disk)
+    disk = Number(disk);
     console.log(typeof disk === "number");
     dartfs = document.getElementById("dartfs").value;
     sysadmin = document.getElementById("sysadmin").checked;
-    cost_per_cpu = 60
-    cost_per_memory = 40
-    cost_per_disk = 2.5
-    cost_per_dartfs = 95
-    sysadmin_cost = 200
+    cost_per_cpu = 60;
+    cost_per_memory = 40;
+    cost_per_disk = 2.5;
+    cost_per_dartfs = 95;
+    sysadmin_cost = 200;
 };
