8d78167d0d326db3b31a1db59db82e0dc59de8e4
Henry Rice <riceh26@hartfordschools.net>
2026-04-29 10:07:44 -0400

    js and html invoice changes

--- invoice/invoice.js
+++ invoice/invoice.js
@@ -1,26 +1,34 @@
-let cpus
-let memory
-let disk
-let dartfs
-let sysadmin
+let cpus = 80
+let memory = 40
+let disk = 2.5
+let dartfs = 95
+let sysadmin = 200
 
 function price_calculation(cpus, memory, disk, dartfs, sysadmin) {
     let cpus_subtotal = 0;
     let memory_subtotal = 0;
     let disk_subtotal = 0;
     let dartfs_subtotal = 0;
-    let sysadmin_subtotal = 200;
+    let sysadmin_subtotal = 0;
 
     if (cpus) {
         cpus_subtotal += cost_per_cpu * cpus;
+        const cpus = Number(document.getElementById("cpus").value);
     }
     if (memory) {
         memory_subtotal += cost_per_gb_memory * memory;
+        const memory = Number(document.getElementById("memory").value);
     }
     if (disk) {
         disk_subtotal += cost_per_gb_disk * disk;
+        const disk = Number(document.getElementById("disk-space").value);
     }
     if (dartfs) {
         dartfs_subtotal += cost_per_tb_dartfs * dartfs;
+        const dartfs = Number(document.getElementById("dartfs").value);
     }
-}
+    if (sysadmin) {
+        sysadmin_subtotal = sysadmin
+        const sysadmin = document.getElementById("sysadmin").checked;
+    }
+    }
